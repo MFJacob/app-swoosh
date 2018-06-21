0f6c31c73ea5eb8e71172ff8a6032bd2b9d68f1b
AmplitudeGroup <amplitudegroup@Maximilians-MacBook.local>
2018-06-21 14:35:41 +0200

    Segue installed

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //that function gets called when pressing on the BACK errow
+    @IBAction func unwindFromSkillVC(undwindSegue: UIStoryboardSegue) {
+        
     }
 
 
